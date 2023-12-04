9c3479beff009544a4800e4b89c4f5cdff084ea9
Spyridon Siafis <sksiafis@gmail.com>
2023-12-04 19:48:18 +0100

    Add files via upload
    R code for analysis and functions used in the R markdown. There were some problems in the util.R and thus, I saved the functions in the analysis.R

---
+++ util/util.R
@@ -0,0 +1,25 @@
+library(meta)
+
+
+forest_presentation<-function(meta_comp, outcome){
+  
+  if(outcome=="efficacy"){
+    meta::forest(meta_comp, 
+                       label.e = unique(meta_comp$data$treat1_new),
+                 label.c=unique(meta_comp$data$treat2_new),
+                       subgroup.hetstat = TRUE, overall.hetstat =FALSE, test.subgroup.random = FALSE,
+                       overall = FALSE, prediction.subgroup = FALSE,
+                       subgroup = TRUE,
+                       label.left = "TAAR1 worse", label.right="TAAR1 better")
+  } else if(outcome=="safety") {
+    meta::forest(meta_comp, prediction = FALSE,
+           label.e = unique(meta_comp$data$treat1_new), label.c=unique(meta_comp$data$treat2_new),
+           label.left = "TAAR1 better",
+           label.right="TAAR1 worse", 
+           subgroup.hetstat = TRUE, overall.hetstat =TRUE, test.subgroup.random = FALSE, test.subgroup.fixed = FALSE,
+           overall = TRUE, prediction.subgroup = FALSE,
+           subgroup = FALSE)
+    
+  }
+  
+}
