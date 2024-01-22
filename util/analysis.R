677883c03125b28334d456bd808923c46a4b0d71
sksiafis <spyridon.siafis@tum.de>
2024-01-22 22:41:16 +0100

    Revision of the analysis.R (potentially few more additions to do)
    1) I used r=0.2 to correct the seTE of the three crossover studies. I used the Elbourne et al 2002 for this. When events = 0 or n-events =0, i added 0.5, otherwise the correction leading to infinity.
    2) I am not sure on how to pool the two doses of TAAR1 of the three-period crossover study of Szabo et al. I considered it like a three-arm study and added the ns and events of the doses of ulotaront (although the same patients in both). There were only data for one dichotomous outcome of adverse events. Is there any other way?
    3) To estimate absolute risks (may be useful for the SoE), I calclulated the pooled placebo response using metaprop. I used "IV" instead of "GLMM" because the latter did not always work (probably few studies and small number of events). I used logit transformation (default).
    4) I used fixed and random effects for all outcomes.
    There are few things to add probably.

--- util/analysis.R
+++ util/analysis.R
@@ -9,13 +9,11 @@ rm(list=ls())
 
 master<-read_xlsx("data/data_2024-01-22_LSR3_H.xlsx")
 
-rob <- master %>% select(study_name, Overall) %>% rename(rob=Overall)
+rob <- master %>% select(study_name, Overall) %>% rename(rob=Overall) %>% unique()
 
-
-master<-master %>% filter(main==1) #Remove some arms that will not be analyze i.e., either components of a larger arm or an arm from a three-phase crossover that is a lower dose of ulotaront and complicates the analysis (few data on side-effects) 
 
 continuous_outcomes_smd<-c("overall", "positive", "negative", "functioning", "cognition", "depression")
-continuous_outcomes_md<-c("weight","prolactin")
+continuous_outcomes_md<-c("weight","prolactin") #qtc interval not used here as data only from a single study that were caclulated using AUC
 dichotomous_outcomes_common<-c("dropout_any", 'dropout_ae', "adverse_event", "response", "relapse",
                                "anticholinertgic_symptom", "anxiety","agitation", "headache", "hypotension", "dizziness",
                                "nausea_vomitting", "sedation", "hyperprolactinemia", "qtc_prolongation",
@@ -25,14 +23,18 @@ dichotomous_outcomes_rare<-c("death", "serious")
 effic_outcomes<-c("overall", "positive", "negative", "functioning", "cognition", "depression", "response", "relapse")
 primary_outcome<-"overall"
 
-meta_outcome<-data.frame(outcome=NA, analysis=NA, comparison=NA, timepoint=NA, k=NA,
-                         sm=NA, TE=NA, seTE=NA)
+meta_outcome<-data.frame(outcome=NA,  comparison=NA, timepoint=NA, k=NA, n=NA,
+                         sm=NA,  
+                         TE.placebo.random=NA, seTE.placebo.random=NA, 
+                         TE.placebo.fixed=NA, seTE.placebo.fixed=NA, 
+                         TE.random=NA, seTE.random=NA, TE.fixed=NA, seTE.fixed=NA)
+
+o<-"dropout_ae"
+time<-"1 day-2 weeks"
+comparison="taar1_vs_placebo"
 
 for(o in c(continuous_outcomes_smd, continuous_outcomes_md, dichotomous_outcomes_common, dichotomous_outcomes_rare)){
   for(time in unique(master$timepoint)){
-  for(pairwise in "yes"){ #pre-post analyses will not be done for the presentation
-    
-    if(pairwise=="yes"){
       
       for(comparison in c("taar1_vs_placebo", "taar1_vs_antipsychotic")){
         
@@ -44,7 +46,7 @@ for(o in c(continuous_outcomes_smd, continuous_outcomes_md, dichotomous_outcomes
           master_i$n<-master[[paste0(o,"_n")]]
           prediction_true=TRUE
           random_true=TRUE
-          fixed_true=FALSE
+          fixed_true=TRUE #present both fixed and random effects due to small number of studies
           approach="Inverse"
           sm_used="MD"
           outcome_type="continuous"
@@ -57,7 +59,7 @@ for(o in c(continuous_outcomes_smd, continuous_outcomes_md, dichotomous_outcomes
           sm_used="SMD"
           prediction_true=TRUE
           random_true=TRUE
-          fixed_true=FALSE
+          fixed_true=TRUE #present both fixed and random effects due to small number of studies
           approach="Inverse"
           outcome_type="continuous"
           
@@ -66,7 +68,7 @@ for(o in c(continuous_outcomes_smd, continuous_outcomes_md, dichotomous_outcomes
           master_i$sd<-NA
           master_i$n<-master$randomized_n #Use the randomized n in the dichotomous outcomes, asssuming missing patients did not have the outcome (post-hoc from protcol regrding safety outcomes but this we usullay do)
           random_true=TRUE
-          fixed_true=FALSE
+          fixed_true=TRUE #present both fixed and random effects due to small number of studies
           prediction_true=TRUE
           approach="Inverse"
           sm_used="OR"
@@ -75,7 +77,7 @@ for(o in c(continuous_outcomes_smd, continuous_outcomes_md, dichotomous_outcomes
           master_i$mean<-master[[paste0(o,"_e")]] #used as n with event 
           master_i$sd<-NA
           master_i$n<-master$randomized_n
-          random_true=FALSE
+          random_true=FALSE #Do not present random-effects in outcomes deemed as rare
           prediction_true=FALSE
           fixed_true=TRUE
           approach="MH"
@@ -92,37 +94,38 @@ for(o in c(continuous_outcomes_smd, continuous_outcomes_md, dichotomous_outcomes
           overall_hetstat_true = FALSE
           test_subgroup_true = FALSE
           overall_true = FALSE
-          subgroup_true=TRUE
+          subgroup_true=TRUE  #present subgroups in all but without testing
         } else {
           left_label="TAAR1 better"
           right_label="TAAR1 worse"
           prediction_subgroup_true=FALSE
           subgroup_hetstat_true= FALSE
           overall_hetstat_true = TRUE
-          test_subgroup_true= FALSE
+          test_subgroup_true= FALSE 
           overall_true = TRUE
-          subgroup_true=FALSE
+          subgroup_true=TRUE #present subgroups in all but without testing
         }
         
         
+        
         if(comparison=="taar1_vs_placebo"){
-          master_i<-master_i %>% filter(comparison_data==pairwise) %>% #Keep arms that can give pairwise comparisons
+          master_i<-master_i %>% 
             filter(timepoint==time) %>%  #keep the timepoint
             filter(!is.na(mean)) %>% #keep only arms with data
-            select(study_name, population, drug_new, mean, sd, n) %>%  #select only the relevant data for the analysis 
+            select(study_name, study_name_drug, crossover_periods, population, drug_new, mean, sd, n) %>%  #select only the relevant data for the analysis 
             filter(drug_new=="TAAR1 agonist" | drug_new=="placebo")  #keep the arms relevant for the comparison 
         } else if(comparison=="taar1_vs_antipsychotic"){
-          master_i<-master_i %>%  filter(comparison_data==pairwise) %>% #Keep arms that can give pairwise comparisons
-              filter(timepoint==time) %>%  #keep the timepoint
+          master_i<-master_i %>%  
+             filter(timepoint==time) %>%  #keep the timepoint
               filter(!is.na(mean)) %>% #keep only arms with data
-             dplyr::select(study_name, population, drug_new, mean, sd, n) %>% 
+             dplyr::select(study_name, study_name_drug, crossover_periods, population, drug_new, mean, sd, n) %>% 
               filter(drug_new=="TAAR1 agonist" | drug_new=="D2 antipsychotic") 
         }
        
         if(o %in% c(continuous_outcomes_md, continuous_outcomes_smd)){
           
           master_pooled_i<-master_i %>%  #pooling the different doses or drugs of the same category (not relevant here)
-            group_by(study_name, drug_new, population)%>%
+            group_by(study_name, study_name_drug, drug_new, population)%>%
             summarise(n_an=sum(n),
                       mean_an=weighted.mean(mean,n), 
                       sd_an=sqrt(((sum((n - 1) * sd^2 + n * mean^2)) - sum(n) * mean_an^2)/(sum(n) -  1)))
@@ -133,7 +136,7 @@ for(o in c(continuous_outcomes_smd, continuous_outcomes_md, dichotomous_outcomes
           master_pooled_i<-master_pooled_i %>% filter(study_name %in% master_pooled_i_studies$study_name) 
           
           if(nrow(master_pooled_i)>1){
-          pairwise_i<-pairwise(data=master_pooled_i, studlab = study_name, treat=drug_new,
+          pairwise_i<-pairwise(data=master_pooled_i, studlab = study_name_drug, treat=drug_new,
                                mean=mean_an, sd=sd_an, n=n_an, sm=sm_used)
           
           pairwise_i<-as.data.frame(pairwise_i) %>% 
@@ -147,7 +150,9 @@ for(o in c(continuous_outcomes_smd, continuous_outcomes_md, dichotomous_outcomes
                    n1_new=ifelse(treat1=="TAAR1 agonist" ,n1, n2),
                    n2_new=ifelse(treat1=="TAAR1 agonist" , n2, n1)) %>%
             mutate(comp=paste0(treat1_new," vs. ", treat2_new)) %>%
-            select(study_name, comp, population, treat1_new, treat2_new, mean1_new, sd1_new, n1_new, mean2_new,  sd2_new, n2_new) %>% 
+            select(study_name, study_name_drug, 
+                   comp, population, treat1_new, treat2_new, mean1_new, sd1_new, n1_new, mean2_new,  sd2_new, n2_new) %>% 
+            mutate(n_total=n1_new + n2_new) %>%
             unique()
           
           if(o %in% primary_outcome){
@@ -158,7 +163,7 @@ for(o in c(continuous_outcomes_smd, continuous_outcomes_md, dichotomous_outcomes
           meta_comp<-metacont(data=pairwise_i,  #method.random.ci = "HK",  adhoc.hakn.ci="se", #reml default for tau, Do not use HK correction due to <5 studies
                              mean.e = mean1_new, sd.e=sd1_new, n.e=n1_new, mean.c = mean2_new, sd.c=sd2_new, n.c=n2_new,
                              sm=sm_used, random=random_true, fixed=fixed_true,    
-                             studlab = study_name, prediction = prediction_true, subgroup = population, 
+                             studlab = study_name_drug, prediction = prediction_true, subgroup = population, 
                              prediction.subgroup = prediction_subgroup_true)
           
           new_master_i_name<- paste0("master_i_", comparison,"_", time,"_",o)  
@@ -173,15 +178,15 @@ for(o in c(continuous_outcomes_smd, continuous_outcomes_md, dichotomous_outcomes
         } else if(o %in% c(dichotomous_outcomes_common, dichotomous_outcomes_rare)){
           
           master_pooled_i<-master_i %>%  #pooling the different doses or drugs of the same category (not relevant here)
-            group_by(study_name, drug_new, population)%>%
+            group_by(study_name, study_name_drug, crossover_periods, drug_new, population)%>%
             summarise(n_an=sum(n),
                       e_an=sum(as.numeric(mean))) %>% 
             unique()
           
           master_pooled_i_studies<-master_pooled_i %>% 
-            select(study_name) %>% group_by(study_name) %>% summarise(count=n()) %>% filter(count!=1) #find studies with only data for one arm 
+            select(study_name_drug) %>% group_by(study_name_drug) %>% summarise(count=n()) %>% filter(count!=1) #find studies with only data for one arm 
           
-          master_pooled_i<-master_pooled_i %>% filter(study_name %in% master_pooled_i_studies$study_name) 
+          master_pooled_i<-master_pooled_i %>% filter(study_name_drug %in% master_pooled_i_studies$study_name_drug) 
         
             if(nrow(master_pooled_i)>1){
          pairwise_i<-pairwise(data=master_pooled_i, studlab = study_name, treat=drug_new,
@@ -195,35 +200,62 @@ for(o in c(continuous_outcomes_smd, continuous_outcomes_md, dichotomous_outcomes
                    n1_new=ifelse(treat1=="TAAR1 agonist" ,n1, n2),
                    n2_new=ifelse(treat1=="TAAR1 agonist" , n2, n1)) %>%
             mutate(comp=paste0(treat1_new," vs. ", treat2_new)) %>%
-            select(study_name, comp, population, treat1_new, treat2_new, event1_new, n1_new, event2_new, n2_new) %>% 
+            select(study_name, study_name_drug, comp, 
+                   population, crossover_periods, treat1_new, treat2_new, event1_new, n1_new, event2_new, n2_new, TE, seTE) %>%
+           mutate(event1_new_corrected=ifelse(event1_new==0, 0.5, event1_new), #To correct for crosssover, i need to add 0.5 when 0.
+                  event2_new_corrected=ifelse(event2_new==0, 0.5, event2_new),
+                  subtr_event1_new_corrected=ifelse(n1_new- event1_new==0, 0.5, n1_new- event1_new),
+                  subtr_event2_new_corrected=ifelse(n2_new- event1_new==0, 0.5, n2_new- event2_new)) %>% 
+           mutate(varTE=ifelse(crossover_periods==1, seTE^2,       #correct for crossover studies using correlation of 0.2 and the formula in Elbourne
+                               seTE^2-0.4*(n1_new+n2_new)/(sqrt(event1_new_corrected*subtr_event1_new_corrected*event2_new_corrected* subtr_event2_new_corrected))), 
+                  n_total=(n1_new+n2_new)/crossover_periods) %>% 
            unique()
           
-         meta_comp<-metabin(data=pairwise_i, #method.random.ci = "HK",  adhoc.hakn.ci="se",#reml default for tau, Do not use HK correction <5 studies
+         if(length(unique(pairwise_i$crossover_periods))==1){
+         meta_comp<-metabin(data=pairwise_i, 
                             event.e = event1_new, n.e=n1_new, event.c = event2_new, n.c=n2_new,
                             sm=sm_used, random=random_true, fixed=fixed_true,  
-                            studlab = study_name, prediction = prediction_true, subgroup=population)
+                            studlab = study_name_drug, prediction = prediction_true, subgroup=population)
+         } else{
+           meta_comp<-metagen(data=pairwise_i, #Present forest plots using crossover corrections
+                              TE = TE, seTE=sqrt(varTE), 
+                              sm=sm_used, random=random_true, fixed=fixed_true,  
+                              studlab = study_name_drug, prediction = prediction_true, subgroup=population)
+            }
          
+         
+         meta_comp_plac<-metaprop(data=pairwise_i, event=event2_new, n=n2_new, method="Inverse", #Inverse instead of GLMM was used because in some occassions GLMM did not run, Logit transformation was used
+                                  random=random_true, fixed=fixed_true,  
+                                  studlab = study_name_drug, prediction = prediction_true, subgroup=population)
          
          new_master_i_name<- paste0("master_i_", comparison,"_", time,"_",o)  
         new_pairwise_i_name <- paste0("pairwise_i_", comparison,"_", time,"_",o)  
          new_meta_comp_name <- paste0("meta_comp_", comparison,"_",time,"_", o)  
          assign( new_master_i_name, master_i)
-         assign( new_pairwise_i_name, pairwise_i)
+         assign(new_pairwise_i_name, pairwise_i)
          assign(new_meta_comp_name, meta_comp)
          
-         if(!o %in% effic_outcomes){
-           if(random_true==TRUE){
-           meta_outcome_i<-data.frame(outcome=o, analysis=pairwise, comparison=comparison,timepoint=time, 
-                                      k=meta_comp$k.all, sm=sm_used, 
-                                      TE=meta_comp$TE.random, seTE=meta_comp$seTE.random)
-           
+         if(o %in% c(continuous_outcomes_md, continuous_outcomes_smd)){
+           meta_outcome_i<-data.frame(outcome=o, comparison=comparison,timepoint=time, 
+                                      k=meta_comp$k.all,
+                                      n=sum(pairwise_i$n_total),
+                                      sm=sm_used, 
+                                      TE.placebo.random=NA, seTE.placebo.random=NA, 
+                                      TE.placebo.fixed=NA, seTE.placebo.fixed=NA, 
+                                      TE.random=meta_comp$TE.random, seTE.random=meta_comp$seTE.random,
+                                      TE.fixed=meta_comp$TE.fixed, seTE.fixed=meta_comp$seTE.fixed)
+         } else{
+           meta_outcome_i<-data.frame(outcome=o, comparison=comparison,timepoint=time, 
+                                     k=meta_comp$k.all,
+                                     n=sum(pairwise_i$n_total),
+                                     sm=sm_used, 
+                                     TE.placebo.random=meta_comp_plac$TE.random, seTE.placebo.random=meta_comp_plac$seTE.random, 
+                                     TE.placebo.fixed=meta_comp_plac$TE.fixed, seTE.placebo.fixed=meta_comp_plac$TE.fixed, 
+                                     TE.random=meta_comp$TE.random, seTE.random=meta_comp$seTE.random,
+                                     TE.fixed=meta_comp$TE.fixed, seTE.fixed=meta_comp$seTE.fixed)
+         }
+         
            meta_outcome<-rbind(meta_outcome, meta_outcome_i)
-           } else if(random_true==FALSE){
-             meta_outcome_i<-data.frame(outcome=o, analysis=pairwise, comparison=comparison,timepoint=time, 
-                                        k=meta_comp$k.all, sm=sm_used, 
-                                        TE=meta_comp$TE.fixed, seTE=meta_comp$seTE.fixed)
-             
-             meta_outcome<-rbind(meta_outcome, meta_outcome_i)
            }
          }
          
@@ -235,10 +267,8 @@ for(o in c(continuous_outcomes_smd, continuous_outcomes_md, dichotomous_outcomes
        
         
       }
-    }
-  }
-  }
-}
+
+  
 
 
 
